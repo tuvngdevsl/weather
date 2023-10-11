18641b3ce2ee07e2f8ec45a3f20a227ad01527e1
tan <dinovu285@gmail.com>
2023-10-11 13:06:38 +0700

    nav bar

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import classNames from 'classnames/bind'
-import styles from './Header.module.scss'
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 const Header = () => {
-    return <header className={cx('wrapper')}> Hello world </header>
-    abc
+    return (
+        <header className={cx('wrapper')}>
+            <nav>
+                <ul>
+                    <li>H.nay</li>
+                    <li>Hàng giờ</li>
+                    <li>10 ngày</li>
+                    <li>Hàng tháng</li>
+                    <li>Cuối tuần</li>
+                    <li>Thêm dự báo
+                        <ul>
+                            <ul>CẢNH BÁO ĐẶC BIỆT
+                                <li>Trình theo dõi dị ứng</li>
+                                <li>Dự báo chất lượng không khí</li>
+                            </ul>
+                            
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
