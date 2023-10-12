d57a1d4ba8d3b190af71223eab024f61f37250b4
tan <dinovu285@gmail.com>
2023-10-12 10:03:35 +0700

    update subheader 12/10 lan 1

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -7,23 +7,36 @@ const cx = classNames.bind(styles);
 const Header = () => {
     return (
         <header className={cx('wrapper')}>
+            <div className={cx('location-temp')}>
+                <span>Hanoi, Vietnam</span>
+                <span>25°C</span>
+            </div>
             <nav>
-                <ul>
-                    <li>H.nay</li>
-                    <li>Hàng giờ</li>
-                    <li>10 ngày</li>
-                    <li>Hàng tháng</li>
-                    <li>Cuối tuần</li>
-                    <li>Thêm dự báo
-                        <ul>
-                            <ul>CẢNH BÁO ĐẶC BIỆT
-                                <li>Trình theo dõi dị ứng</li>
-                                <li>Dự báo chất lượng không khí</li>
-                            </ul>
-                            
-                        </ul>
-                    </li>
-                </ul>
+                <div>
+                    <a>H.nay</a>
+                    <a>Hàng giờ</a>
+                    <a>10 ngày</a>
+                    <a>Hàng tháng</a>
+                    <a>Cuối tuần</a>
+                    <div>
+                        <button>
+                            Thêm dự báo
+                        </button>
+                        <nav>
+                            <div>
+                                <div>
+                                    <nav>
+                                        <h3>CẢNH BÁO ĐẶC BIỆT</h3>
+                                        <div>
+                                            <a>Trình theo dõi dị ứng</a>
+                                            <a>Dự báo chất lượng không khí</a>
+                                        </div>
+                                    </nav>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
             </nav>
         </header>
     );
